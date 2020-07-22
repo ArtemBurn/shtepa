d04af49d8fc12a583ab1f431caf2aeb6195ee5e3
burn <artem.shtepa.93@gmail.com>
2020-07-22 21:54:30 +0300

    make it better2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 	def index
         @task = Task.new
-        @tasks = Task.all
+        @tasks = Task.all.order(created_at: :desc)
     end
      
     def create
@@ -12,6 +12,26 @@ class TasksController < ApplicationController
            redirect_to root_path
        end
         
+        def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
+  
+  redirect_to root_path
+end
+        
+        def edit
+    @task = Task.find(params[:id])
+end
+        def update
+    @task = Task.find(params[:id])
+ 
+    if @task.update(tasks_params)
+        redirect_to root_path
+    else
+        redirect_to root_path
+    end
+end
+
     end
      
     private
