4899cca49f962e810d8b89f493f1fc60ab178bf7
burn <artem.shtepa.93@gmail.com>
2020-07-22 22:27:00 +0300

    lastchanse

--- db/schema.rb
+++ db/schema.rb
@@ -15,24 +15,6 @@ ActiveRecord::Schema.define(version: 2020_07_22_181959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "families", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "jobs", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "others", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "text"
